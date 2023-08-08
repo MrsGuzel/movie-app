ed0cfdf554c9f7b9cab10f1934cc0baa804aaaa4
MrsGuzel <aleevgzll@gmail.com>
2023-08-08 19:42:26 +0200

    add: navigate

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,14 +1,20 @@
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth'
 import React, { createContext } from 'react'
 import { auth } from '../auth/firebase'
+import { useNavigate } from 'react-router-dom';
+
 
 export const AuthContext  = createContext()
 
 const AuthContextProvider = ({children}) => {
+let navigate = useNavigate();
+
     const createUser = async (email, password) => {
         try {
             let userCredential = await createUserWithEmailAndPassword(auth, email, password)
             console.log(userCredential);
+            navigate("/")
+
         } catch (error) {
             console.log(error.message);
         }
@@ -17,7 +23,8 @@ const AuthContextProvider = ({children}) => {
 
     const signIn = async (email, password) => {
         try {
-           let userCredential = await signInWithEmailAndPassword(auth, email, password)
+           let userCredential = await signInWithEmailAndPassword(auth, email, password);
+           navigate("/");
            console.log(userCredential);
             
         } catch (error) {
