3a2d0be48830b6a21c386e5e03680218161a8dbf
MrsGuzel <aleevgzll@gmail.com>
2023-08-09 18:31:38 +0200

    add: movieContext

---
+++ src/context/MovieContext.js
@@ -0,0 +1,10 @@
+import React, { createContext } from 'react'
+
+export const MovieContext= createContext()
+const MovieContextProvider = ({children}) => {
+  return (
+    <MovieContext.Provider value={{null}}>{children}</MovieContext.Provider>
+  )
+}
+
+export default MovieContextProvider
